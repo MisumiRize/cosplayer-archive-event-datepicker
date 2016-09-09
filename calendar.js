1f436fb1879c6a255559129cf7ee79c7c2bb2288
Misumi Rize <r@ayase-e.li>
2016-09-09 13:09:12 +0900

    add scroll detection

--- calendar.js
+++ calendar.js
@@ -1,12 +1,25 @@
+import moment from 'moment'
 import RCCalendar from 'rc-calendar'
 import React from 'react'
 
 export default class Calendar extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {value: moment()}
+  }
+
+  componentDidMount() {
+    this.props.currentDay.onValue(d => this.setState({value: d}))
+  }
+
   render() {
-    return <RCCalendar onSelect={this.props.onSelect} />
+    return <RCCalendar
+      value={this.state.value}
+      onSelect={this.props.onSelect} />
   }
 }
 
 Calendar.propTypes = {
+  currentDay: React.PropTypes.object,
   onSelect: React.PropTypes.func
 }
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import Kefir from 'kefir'
 import moment from 'moment'
 import React from 'react'
 import ReactDOM from 'react-dom'
@@ -9,18 +10,36 @@ wrapper.className = 'datepicker--wrapper'
 document.body.appendChild(wrapper)
 
 const els = document.querySelectorAll('.black_mui16150')
+const y = window.pageYOffset
 const days = Array.prototype.map.call(els, (el) => {
-  return moment(el.innerText.replace(/（.）/, ''), 'MM月DD日')
+  return {
+    y: el.getBoundingClientRect().top + y,
+    day: moment(el.innerText.replace(/（.）/, ''), 'MM月DD日')
+  }
 })
 
-const navigateToDay = (day) => {
-  const position = days.findIndex((d) => {
-    return d.isSame(day, 'day')
-  })
+function navigateToDay(day) {
+  const position = days.findIndex(d => d.day.isSame(day, 'day'))
   if (position > -1) {
-    const rect = els[position].getBoundingClientRect()
-    window.scrollTo(0, rect.top + window.pageYOffset)
+    window.scrollTo(0, days[position].y)
   }
 }
 
-ReactDOM.render(<Calendar onSelect={navigateToDay}/>, wrapper)
+const currentDay = Kefir.fromEvents(window, 'scroll', () => {
+  return window.pageYOffset
+})
+  .map(y => {
+    return days.find((d, i) => {
+      if (i == days.length - 1) {
+        return true
+      }
+      const bottom = window.innerHeight + y
+      return d.y <= bottom && days[i + 1].y > bottom
+    })
+  })
+  .skipDuplicates()
+  .map(d => d.day)
+
+ReactDOM.render(<Calendar
+  currentDay={currentDay}
+  onSelect={navigateToDay} />, wrapper)
